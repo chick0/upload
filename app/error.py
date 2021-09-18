6a0ab1d2430fd07c3dbefab629cd663828a1c97e
chick_0 <chick_0@ch1ck.xyz>
2021-09-18 17:43:09 +0900

    오류 페이지 수정

--- app/error.py
+++ app/error.py
@@ -7,45 +7,50 @@ from app.template_filter import display_size
 from app.models import Error
 
 
-def file_is_empty(e):
-    error = Error(
-        title="파일 업로드 실패",
-        subtitle="업로드 할 파일을 발견하지 못했습니다."
-    )
-
+def forbidden(e):
     return render_template(
         "error.html",
-        error=error
-    ), 400
+        error=Error(
+            title="403",
+            subtitle="권한이 부족합니다."
+        )
+    ), 403
 
 
 def page_not_found(e):
-    error = Error(
-        title="404",
-        subtitle="해당 파일을 찾을 수 없습니다."
-    )
-
     return render_template(
         "error.html",
-        error=error
+        error=Error(
+            title="404",
+            subtitle="해당 파일을 찾을 수 없습니다."
+        )
     ), 404
 
 
 def file_is_too_big(e):
     max_size = display_size(current_app.config['MAX_CONTENT_LENGTH'])
-    error = Error(
-        title="파일 업로드 실패",
-        subtitle=f"업로드 가능한 가장 큰 파일의 크기는 <b>{max_size}</b>입니다."
-    )
-
     return render_template(
         "error.html",
-        error=error
+        error=Error(
+            title="파일 업로드 실패",
+            subtitle=f"업로드 가능한 가장 큰 파일의 크기는 <b>{max_size}</b>입니다."
+        )
     ), 413
+
+
+def file_is_empty(e):
+    return render_template(
+        "error.html",
+        error=Error(
+            title="파일 업로드 실패",
+            subtitle="업로드 할 파일을 발견하지 못했습니다."
+        )
+    ), 400
 
 
 # error map
 error_map = {
+    403: forbidden,
     404: page_not_found,
     413: file_is_too_big,
 
