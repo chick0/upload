04dd6b046e12afc3bb3a75ad99be1f7a3f88f2e6
chick_0 <chick_0@ch1ck.xyz>
2022-06-02 00:52:28 +0900

    ui update

--- app/views/file.py
+++ app/views/file.py
@@ -3,8 +3,9 @@ from json import loads
 from flask import Blueprint
 from flask import g
 from flask import abort
+from flask import request
 from flask import session
-from flask import Response
+from flask import url_for
 from flask import render_template
 
 from app import redis
@@ -32,24 +33,10 @@ def show(file_id: str):
         "file/show.html",
         file_id=file_id,
         file=file,
-    )
-
-
-@bp.get("/<string:file_id>/checksums.txt")
-def checksums(file_id: str):
-    from_redis = redis.get(f"chick0/upload:{file_id}")
-    if from_redis is None:
-        return abort(404)
-
-    from_redis = loads(from_redis)
-    file = File(*from_redis)
-
-    return Response(
-        response="\n".join([
-            file.name,
-            f"sha256sum {file.sha256}",
-        ]),
-        mimetype="text/plain"
+        download_url="{host}{path}".format(
+            host="{scheme}://{host}".format(scheme=request.scheme, host=request.host),
+            path=url_for("download.file", file_id=file_id, fake=file.name),
+        )
     )
 
 
